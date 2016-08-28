7a1ef70900022c83fbf9f976890e16b37806eda6
Christopher David Desjardins <cddesjardins@gmail.com>
2016-08-28 11:40:53 -0500

    initial REPM
    includes interest and wiscsem data sets

---
+++ R/data.R
@@ -0,0 +1,68 @@
+#' WISC-R scores on 175 children
+#'
+#' The WISC-R subscale data from Tabacknick & Fidell, 3rd ed "Using Multivariate Statistics". The suscale scores come for the second version of the Wechsler Intelligence Test for Children. The students were school-aged children previously diagnosed as learning-disabled. The data were collected between 1972 and 1984.
+#'
+#' @docType data
+#' @keywords dataset
+#' @format A data frame with 175 rows and 13 variables:
+#' \describe{
+#'   \item{info}{Information}
+#'   \item{comp}{Comprehension}
+#'   \item{arith}{Arithmetic knowledge}
+#'   \item{simil}{Similarities}
+#'   \item{vocab}{Vocabulary}
+#'   \item{digit}{Digit span}
+#'   \item{pictcomp}{Picture comprehension}
+#'   \item{parang}{Picture arrangement}
+#'   \item{block}{Block design}
+#'   \item{object}{Object assembly}
+#'   \item{coding}{Coding}
+#' }
+#' @source \url{http://psych.colorado.edu/~carey/Courses/PSYC7291/ClassDataSets.htm}
+"wiscsem"
+
+#' Fabricated cognitive, personality, and vocational interest inventory
+#'
+#' The data come from a fabricated cognitive, personality, and vocational interested inventory. This data set can be used to demonstrate regression and structural equation modeling.
+#'
+#' @docType data
+#' @keywords dataset
+#' @format A data frame with 250 rows and 33 variables:
+#' \describe{
+#'  \item{gender}{1 is female and 2 is male}
+#'  \item{educ}{Years of education}
+#'  \item{age}{Age, in years}
+#'  \item{vocab}{Vocabulary test}
+#'  \item{reading}{Reading comprehension}
+#'  \item{sentcomp}{Sentence completion}
+#'  \item{mathmtcs}{Mathematics}
+#'  \item{geometry}{Geometry}
+#'  \item{analyrea}{Analytical reasoning}
+#'  \item{socdom}{Social dominance}
+#'  \item{sociabty}{Sociability}
+#'  \item{stress}{Stress reaction}
+#'  \item{worry}{Worry scale}
+#'  \item{impulsve}{Impulsivity}
+#'  \item{thrillsk}{Thrill-seeking}
+#'  \item{carpentr}{Carpentry}
+#'  \item{forestr}{Forest ranger}
+#'  \item{morticin}{Mortician}
+#'  \item{policemn}{Police}
+#'  \item{fireman}{Fireman}
+#'  \item{salesrep}{Sales representative}
+#'  \item{teacher}{Teacher}
+#'  \item{busexec}{Business executive}
+#'  \item{stockbrk}{Stock broker}
+#'  \item{artist}{Artist}
+#'  \item{socworkr}{Social worker}
+#'  \item{truckdvr}{Truck driver}
+#'  \item{doctor}{Doctor}
+#'  \item{clergymn}{Clergyman}
+#'  \item{lawyer}{Lawyer}
+#'  \item{actor}{Actor}
+#'  \item{archtct}{Architect}
+#'  \item{landscpr}{Landscaper}
+#' }
+#' @source \url{http://psych.colorado.edu/~carey/Courses/PSYC7291/ClassDataSets.htm}
+"interest"
+
